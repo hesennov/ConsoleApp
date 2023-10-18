e7682ee9df2fe52dcd15d80f7d48ad1041058e20
hesennov <hasanhasanov345@gmail.com>
2023-10-18 07:55:34 +0400

    'test'

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Boolean arrs = false;
+            //if (arrs)
+            //{
+            //    Console.WriteLine("Hello world gagam");
+
+            //}else
+            //{
+            //    Console.WriteLine(arrs);
+
+            //}
+            //Console.ReadLine();
+            Console.WriteLine("adinizi girin");
+            string name = Console.ReadLine();
+            /*Console.WriteLine("your's names"+ name)*/;
+            Console.WriteLine($"welcome  {name}");
+
+            Console.ReadLine();
         }
     }
 }
