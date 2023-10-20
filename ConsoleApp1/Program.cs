25d2fca17c2ab139e43ffdc937055c9e6ac962d2
hesennov <hasanhasanov345@gmail.com>
2023-10-20 18:31:51 +0400

    sas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -21,12 +21,33 @@ namespace ConsoleApp1
 
             //}
             //Console.ReadLine();
-            Console.WriteLine("adinizi girin");
-            string name = Console.ReadLine();
+            //Console.WriteLine("adinizi girin");
+            //string name = Console.ReadLine();
             /*Console.WriteLine("your's names"+ name)*/;
-            Console.WriteLine($"welcome  {name}");
+            //Console.WriteLine($"welcome  {name}");
+            //a
+            //Console.ReadLine();
+            Console.WriteLine("notunu gir");
+            int first_note = Convert.ToInt32(Console.ReadLine());
+            int second_note=Convert.ToInt32(Console.ReadLine());
+            int third_note =Convert.ToInt32(Console.ReadLine());
 
+            int sonuc = (first_note + second_note + third_note) / 3;
+
+            if(80 < sonuc && sonuc <= 100)
+            {
+                Console.WriteLine("A+");
+            }else if(60 < sonuc && sonuc <=80){
+                Console.WriteLine("A");
+            }else if(40 < sonuc && 60 >= sonuc)
+            {
+                Console.WriteLine("B+");
+            }else if(40 > sonuc)
+            {
+                Console.WriteLine("F");
+            }
             Console.ReadLine();
         }
+        
     }
 }
