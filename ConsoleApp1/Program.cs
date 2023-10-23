30eb04d4598c9037186d434943bee292e30f5d86
hesennov <hasanhasanov345@gmail.com>
2023-10-23 13:55:40 +0400

    d

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -111,15 +111,23 @@ namespace ConsoleApp1
             //} while (a <= 100);
             //Console.WriteLine(toplam);
             //Console.ReadLine();
-            for(int i=1; i<=10; i++)
-            {
-                if (i == 5)
-                {
-                    //break;
-                    continue;
-                }
-                Console.WriteLine(i);
+            //for(int i=1; i<=10; i++)
+            //{
+            //    if (i == 5)
+            //    {
+            //        //break;
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
                 
+            //}
+            for (int i = 1; i < 10; i++)
+            {
+                for(int j = 1; j<10;  j++)
+                {
+                    Console.WriteLine(i+"x"+j+"="+(i*j));
+                }
+                Console.WriteLine("");
             }
             Console.ReadLine();
         }
